169f27e297796fc1ef701330164e46853cbe6566
Obinna Akuma <akumavincent140@gmail.com>
2023-09-20 20:44:49 +0100

    Fixed Pending Errors On Event Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -36,9 +36,7 @@ android {
 
     buildFeatures {
         compose = true
-    }
 
-    buildFeatures{
         viewBinding = true
     }
 
@@ -52,7 +50,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraint layout:constraint layout:2.1.4")
+    implementation("androidx.compose.ui:ui:1.5.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
