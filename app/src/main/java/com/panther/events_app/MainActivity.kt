8921220dc612c5ed1aaa03d193c26863b6945f2f
Obinna Akuma <akumavincent140@gmail.com>
2023-09-20 21:02:08 +0100

    Added Fragments

--- app/src/main/java/com/panther/events_app/MainActivity.kt
+++ app/src/main/java/com/panther/events_app/MainActivity.kt
@@ -2,10 +2,16 @@ package com.panther.events_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, CreateEventFragment()).commit()
     }
 }
