abf881d2e31c11a24f6b5a208a755244f0d34175
Techsultan <salamsultan09@gmail.com>
2023-09-21 11:10:41 +0100

    moved classes to different packages

--- app/src/main/java/com/panther/events_app/MainActivity.kt
+++ app/src/main/java/com/panther/events_app/MainActivity.kt
@@ -2,17 +2,14 @@ package com.panther.events_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import com.panther.events_app.fragment.createevent.CreateEventFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentManager: FragmentManager = supportFragmentManager
+        /*val fragmentManager: FragmentManager = supportFragmentManager
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainer, CreateEventFragment()).commit()
+        fragmentTransaction.replace(R.id.fragmentContainer, CreateEventFragment()).commit()*/
     }
 }
