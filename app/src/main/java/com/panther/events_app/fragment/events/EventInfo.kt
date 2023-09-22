8ae034892d7c8b34e10196e5d2323cc3e8315f6c
Joseph <j.t.cartoonist@gmail.com>
2023-09-22 18:50:42 +0100

    added comment box, image selector and mic icon ...

--- app/src/main/java/com/panther/events_app/fragment/events/EventInfo.kt
+++ app/src/main/java/com/panther/events_app/fragment/events/EventInfo.kt
@@ -1,9 +1,14 @@
 package com.panther.events_app.fragment.events
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.panther.events_app.databinding.FragmentEventInfoBinding
 import com.panther.events_app.fragment.events.adapters.EventCommentsAdapter
@@ -27,6 +32,33 @@ class EventInfo : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.eventCommentsRv.adapter = commentsAdapter
         commentsAdapter.submitList(getMyPeopleEventComments())
+
+        binding.addImageBtn.setOnClickListener {
+            selectImage()
+        }
+
+    }
+
+    private val requestAccountImgPicker =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val data = it.data?.data
+                data?.let { uri ->
+                   //do something
+                }
+                return@registerForActivityResult
+            }
+            Toast.makeText(requireContext(), "Unable to select image", Toast.LENGTH_SHORT).show()
+        }
+
+    private fun selectImage() {
+        val intent = Intent(
+            Intent.ACTION_GET_CONTENT,
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        ).apply {
+            type = "image/*"
+        }
+        requestAccountImgPicker.launch(intent)
 
     }
 }
