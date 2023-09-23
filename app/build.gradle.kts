efbb864c8cf5a0b3f6cb2b25fadc2f7c853839ea
Richard <okaforichard2k17@gmail.com>
2023-09-23 12:04:58 +0100

    added navigation to add events screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     implementation("androidx.compose.ui:ui-tooling-preview-android:1.5.1")
     implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.tracing:tracing-perfetto-handshake:1.0.0-rc01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
