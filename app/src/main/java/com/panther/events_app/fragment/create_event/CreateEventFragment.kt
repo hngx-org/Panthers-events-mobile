69cdc854c7ef33d326c6e45a3846af24f6c611da
Obinna Akuma <akumavincent140@gmail.com>
2023-09-24 06:09:34 +0100

    Added Done Button & Backstack

--- app/src/main/java/com/panther/events_app/fragment/create_event/CreateEventFragment.kt
+++ app/src/main/java/com/panther/events_app/fragment/create_event/CreateEventFragment.kt
@@ -5,16 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.panther.events_app.R
+import android.widget.Toast
+import com.panther.events_app.databinding.FragmentCreateEventBinding
 
 class CreateEventFragment : Fragment() {
 
+    private var binding: FragmentCreateEventBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_event, container, false)
+        binding = FragmentCreateEventBinding.inflate(inflater, container, false)
+
+
+        binding!!.btnBack.setOnClickListener {
+
+            val fragmentManager = requireActivity().supportFragmentManager
+
+            fragmentManager.popBackStack()
+        }
+
+
+        return binding?.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 
 }
